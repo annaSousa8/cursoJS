c9cbb1705e4f9101e4c451756435b05f66bb9e52
annaSousa8 <annajuliasousa174@gmail.com>
2026-04-28 13:30:00 -0400

    Aula 34: addEventListener - adicionando eventos

---
+++ aula34.js
@@ -0,0 +1,23 @@
+//addEventListener
+
+//const c1=document.getElementById("c1")
+//const c1=document.querySelector("#c1")
+const cursos=[...document.querySelectorAll(".curso")]
+
+cursos.map((el)=>{
+    el.addEventListener("click", (evt)=>{
+        const el=evt.target
+        el.classList.add("destaque")
+        console.log(el.innerHTML + " elemento foi clicado")
+    })
+})
+
+// function msg(){
+//     alert("clicou")
+// }
+
+// c1.addEventListener("click", (evt)=>{
+//     const el=evt.target
+//     el.classList.add("destaque")
+// })
+
